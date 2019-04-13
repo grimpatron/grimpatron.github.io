0d83fa78072acf10d839a0784ca8018d82134456
grimpatron <43726927+grimpatron@users.noreply.github.com>
2019-04-13 11:49:04 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,40 +1,50 @@
-var easyWordsStr = "1 be[bi] быть was,were been 2 have[həv] иметь had had 3 do[du] делать did done 4 say[seɪ] сказать,говорить said said 5 go[ɡoʊ] идти went gone 6 get[ɡet] получать got got/gotten 7 make[meɪk] делать,вносить made made 8 know[noʊ] знать knew known 9 think[θɪŋk] думать thought thought 10 take[teɪk] брать,принять took taken 11 see[siː] видеть saw seen 12 come[kʌm] приходить came come 13 want[wɑːnt] хотеть wanted wanted 14 use[juːz] использовать used used 16 give[ɡɪv] давать gave given 18 work[wɜːrk] работать worked worked 22 need[niːd] нуждаться needed needed 23 feel[fiːl] чувствовать felt felt 32 help[help] помогать helped helped 34 hear[hɪr] слышать heard heard 35 play[pleɪ] играть played played 36 run[rʌn] бежать ran run 37 move[muːv] двигаться,переезжать moved moved 38 live[lɪv] жить lived lived 42 write[raɪt] писать wrote written 43 sit[sɪt] сидеть sat sat 51 learn[lɜːrn] учить learnt/learned learnt/learned 59 speak[spiːk] говорить spoke spoken 60 read[riːd] читать read read 63 open[ˈoʊpən] открывать opened opened 65 win[wɪn] побеждать,выигрывать won won 68 remember[rɪˈmembər] помнить remembered remembered 73 die[daɪ] умирать died died 80 kill[kɪl] убивать killed killed 111 sing[sɪŋ] петь sang sung 117 shoot[ʃuːt] стрелять shot shot 120 fly[flaɪ] летать flew flown 122 sleep[sliːp] спать slept slept 151 look[lʊk] смотреть looked looked 159 jump[dʒʌmp] прыгать jumped jumped";
-var mediumWordsStr = "54 understand (понимать) understood understood 75 build (строить) built built 78 cut (резать) cut cut 85 return (возвращаться) returned returned 100 choose (выбирать) chose chosen 30 begin (начинать) began begun 33 show (показывать) showed shown 39 believe (верить) believed believed 49 continue (продолжать) continued continued 52 change (менять) changed changed 56 follow (следовать) followed followed 58 create (создавать) created created 66 teach (учить,преподавать) taught taught 87 hope (надеяться) hoped hoped 90 break (ломать) broke broken 96 eat есть(кушать) ate eaten 21 ask (спросить;просить;задать) asked asked 41 happen (случаться) happened happened 45 lose (терять,проигрывать) lost lost 46 pay (платить) paid paid 48 include (включать) included included 71 buy (покупать) bought bought 77 fall (падать) fell fallen 83 sell (продавать) sold sold 98 catch (ловить) caught caught 101 hold держать held held 114 shake трясти shook shaken 118 drink пить drank drunk 19 call (звонить,назвать,звать) called called 20 try (попробывать,пытаться) tried tried 24 become (становиться) became become 28 keep (держать,сохранять) kept kept 55 watch (смотреть) watched watched 62 grow (расти) grew grown 64 walk (идти) walked walked 72 serve (служить) served served 79 reach (достигать) reached reached 82 pass (пройти,миновать) passed passed 84 decide (решать) decided decided 92 agree (соглашаться) agreed agreed 93 support (поддерживать) supported supported 97 cover (покрывать) covered covered 104 drive вести_машину drove driven 109 forget забывать forgot forgotten 17 tell (сказать;рассказывать) told told 26 put (класть;положить) put put 29 let (позволять) let let 47 meet (встречать) met met 50 set (устанавливать;ставить) set set 53 lead (вести) led led 86 explain (объяснять) explained explained 88 develop (разрабатывать,развивать) developed developed 89 carry (везти;нести) carried carried 99 draw (рисовать) drew drawn 105 wear носить(одежду) wore worn 107 throw бросать threw thrown 108 deal иметь_дело dealt dealt 112 strike ударять;бить struck struck 115 ride ездить;ездить(верхом) rode ridden 128 hide прятаться hid hidden 129 blow дуть blew blown 132 wake просыпаться,будить woke woken 137 steal красть stole stolen 25 leave (оставлять,уйти,покинуть) left left 40 bring (приносить) brought brought 44 stand (стоять) stood stood 95 produce (производить) produced produced 102 lie лежать lay lain 110 fight драться,бороться fought fought 116 feed кормить fed fed 123 cost стоить cost cost 136 shut закрывать shut shut 15 find (находить) found found 31 seem (казаться) seemed seemed 94 hit (ударять) hit hit 127 cast брать,кидать cast cast 138 tear рвать tore torn 140 ring звонить rang rung 148 shine светить,сиять shone shone 158 add добавлять added added";
-var hardWordsStr = "27 mean (значить;означать;иметь_в_виду) meant meant 57 stop (остановить,прекращать) stopped stopped 61 spend (тратить,проводить(время)) spent spent 67 offer (предлагать) offered offered 69 consider (считать) considered considered 70 appear (появляться) appeared appeared 74 send (посылать;отправлять) sent sent 76 stay (оставаться) stayed stayed 81 raise (поднимать;повысить) raised raised 91 receive (получать) received received 103 rise подниматься rose risen 106 seek искать sought sought 113 hang вешать hung hung 119 arise возникать arose arisen 121 spread распространяться spread spread 124 beat бить beat beaten 125 light освещать,зажигать lit lit 126 bind вязать,связывать bound bound 120 swim плавать swam swum 131 bend сгибать bent bent 133 stick придерживаться,держаться stuck stuck 134 sweep подметать swept swept 135 undertake проводить,осуществлять undertook undertaken 139 hurt повредить,ранить hurt hurt 141 lay положить,класть laid laid 142 bear нести,выносить bore born 143 swing заскочить,заехать,качаться swung swung 144 lend одалживать lent lent 145 sink тонуть,потопить sank sunk 146 overcome преодолеть,побороть overcame overcome 147 freeze замерзать froze frozen 149 withdraw снять,вывести withdrew withdrawn 150 confirm подтверждать,утверждать confirmed confirmed 152 close закрывать closed closed 153 remind напоминать reminded reminded 154 allow позволять,разрешать allowed allowed 155 prohibit запрещать prohibited prohibited 156 thank благодарить thanked thanked 157 provide представить,снабжать provided provided 160 solve решать(проблему) solved solved 161 love любить loved loved 162 discuss обсуждать discussed discussed 163 achieve достигать achieved achieved 164 avoid избегать avoided avoided 165 prepare готовить,подготавливать prepared prepared 166 suggest предлагать,советовать suggested suggested 167 depend зависеть depended depended 168 translate переводить translated translated 169 approve одобрять approved approved 170 cook готовить_еду cooked cooked";
+var easyWordsStr = "1 be[bi] быть was,were been 2 have[həv] иметь had had 3 do[du] делать did done 4 say[seɪ] сказать,говорить said said 5 go[ɡoʊ] идти went gone 6 get[ɡet] получать got got/gotten 7 make[meɪk] делать,вносить made made 8 know[noʊ] знать knew known 9 think[θɪŋk] думать thought thought 10 take[teɪk] брать,принять took taken 11 see[siː] видеть saw seen 12 come[kʌm] приходить came come 13 want[wɑːnt] хотеть wanted wanted 14 use[juːz] использовать used used 16 give[ɡɪv] давать gave given 18 work[wɜːrk] работать worked worked 21 ask[æsk] спросить,просить,задать asked asked 22 need[niːd] нуждаться needed needed 23 feel[fiːl] чувствовать felt felt 30 begin[bɪˈɡɪn] начинать began begun 32 help[help] помогать helped helped 33 show[ʃoʊ] показывать showed shown 34 hear[hɪr] слышать heard heard 35 play[pleɪ] играть played played 36 run[rʌn] бежать ran run 37 move[muːv] двигаться,переезжать moved moved 38 live[lɪv] жить lived lived 39 believe[bɪˈliːv] верить believed believed 42 write[raɪt] писать wrote written 43 sit[sɪt] сидеть sat sat 45 lose[luːz] терять,проигрывать lost lost 51 learn[lɜːrn] учить learnt/learned learnt/learned 54 understand[ˌʌndərˈstænd] понимать understood understood 56 follow[ˈfɑːloʊ] следовать followed followed 58 create[kriˈeɪt] создавать created created 59 speak[spiːk] говорить spoke spoken 60 read[riːd] читать read read 62 grow[ɡroʊ] расти grew grown 63 open[ˈoʊpən] открывать opened opened 65 win[wɪn] побеждать,выигрывать won won 68 remember[rɪˈmembər] помнить remembered remembered 73 die[daɪ] умирать died died 75 build[bɪld] строить built built 77 fall[fɔːl] падать fell fallen 78 cut[kʌt] резать cut cut 80 kill[kɪl] убивать killed killed 85 return[rɪˈtɜːrn] возвращаться returned returned 87 hope[hoʊp] надеяться hoped hoped 96 eat[iːt] есть(кушать) ate eaten 111 sing[sɪŋ] петь sang sung 114 shake[ʃeɪk] трясти shook shaken 117 shoot[ʃuːt] стрелять shot shot 118 drink[drɪŋk] пить drank drunk 120 fly[flaɪ] летать flew flown 122 sleep[sliːp] спать slept slept 132 wake[weɪk] просыпаться,будить woke woken 151 look[lʊk] смотреть looked looked 152 close[kloʊz] закрывать closed closed 159 jump[dʒʌmp] прыгать jumped jumped 161 love[lʌv] любить loved loved";
+var mediumWordsStr = "100 choose (выбирать) chose chosen 49 continue (продолжать) continued continued 52 change (менять) changed changed 66 teach (учить,преподавать) taught taught 90 break (ломать) broke broken 41 happen (случаться) happened happened 46 pay (платить) paid paid 48 include (включать) included included 71 buy (покупать) bought bought 83 sell (продавать) sold sold 98 catch (ловить) caught caught 101 hold держать held held 19 call (звонить,назвать,звать) called called 20 try (попробывать,пытаться) tried tried 24 become (становиться) became become 28 keep (держать,сохранять) kept kept 55 watch (смотреть) watched watched 64 walk (идти) walked walked 72 serve (служить) served served 79 reach (достигать) reached reached 82 pass (пройти,миновать) passed passed 84 decide (решать) decided decided 92 agree (соглашаться) agreed agreed 93 support (поддерживать) supported supported 97 cover (покрывать) covered covered 104 drive вести_машину(водить) drove driven 109 forget забывать forgot forgotten 17 tell (сказать;рассказывать) told told 26 put (класть;положить) put put 29 let (позволять) let let 47 meet (встречать) met met 50 set (устанавливать;ставить) set set 53 lead (вести) led led 86 explain (объяснять) explained explained 88 develop (разрабатывать,развивать) developed developed 89 carry (везти;нести) carried carried 99 draw (рисовать) drew drawn 105 wear носить(одежду) wore worn 107 throw бросать threw thrown 108 deal иметь_дело dealt dealt 112 strike ударять;бить struck struck 115 ride ездить(верхом) rode ridden 128 hide прятаться hid hidden 129 blow дуть blew blown 137 steal красть stole stolen 25 leave (оставлять,уйти,покинуть) left left 40 bring (приносить) brought brought 44 stand (стоять) stood stood 95 produce (производить) produced produced 102 lie лежать lay lain 110 fight драться,бороться fought fought 116 feed кормить fed fed 123 cost стоить cost cost 136 shut закрывать shut shut 15 find (находить) found found 31 seem (казаться) seemed seemed 94 hit (ударять) hit hit 127 cast брать,кидать cast cast 138 tear рвать tore torn 140 ring звонить rang rung 148 shine светить,сиять shone shone 158 add добавлять added added 27 mean (значить;означать;иметь_в_виду) meant meant 67 offer (предлагать) offered offered 69 consider (считать) considered considered 103 rise подниматься rose risen 124 beat бить beat beaten 147 freeze замерзать froze frozen 150 confirm подтверждать,утверждать confirmed confirmed 156 thank благодарить thanked thanked";
+var hardWordsStr = "57 stop (остановить,прекращать) stopped stopped 61 spend (тратить,проводить(время)) spent spent 70 appear (появляться) appeared appeared 74 send (посылать;отправлять) sent sent 76 stay (оставаться) stayed stayed 81 raise (поднимать;повысить) raised raised 91 receive (получать) received received 106 seek стремится,искать sought sought 113 hang вешать hung hung 119 arise возникать arose arisen 121 spread распространяться spread spread 125 light освещать,зажигать lit lit 126 bind вязать,связывать bound bound 120 swim плавать swam swum 131 bend сгибать bent bent 133 stick придерживаться,держаться stuck stuck 134 sweep подметать swept swept 135 undertake проводить,осуществлять undertook undertaken 139 hurt повредить,ранить hurt hurt 141 lay положить,класть laid laid 142 bear нести,выносить bore born 143 swing заскочить,заехать,качаться swung swung 144 lend одалживать lent lent 145 sink тонуть,потопить sank sunk 146 overcome преодолеть,побороть overcame overcome 149 withdraw снять,вывести withdrew withdrawn 153 remind напоминать reminded reminded 154 allow позволять,разрешать allowed allowed 155 prohibit запрещать prohibited prohibited 157 provide представить,снабжать provided provided 160 solve решать(проблему) solved solved 162 discuss обсуждать discussed discussed 163 achieve достигать achieved achieved 164 avoid избегать avoided avoided 165 prepare готовить,подготавливать prepared prepared 166 suggest предлагать,советовать suggested suggested 167 depend зависеть depended depended 168 translate переводить translated translated 169 approve одобрять approved approved 170 cook готовить_еду cooked cooked 171 study учиться studied studied 172 answer отвечать answered answered 173 smile улыбаться smiled smiled 174 repeat повторять repeated repeated 175 join присоединять(ся) joined joined 176 end заканчивать ended ended 177 order заказывать ordered ordered 178 plan планировать planned planned 179 dance танцевать danced danced 180 start начинать started started";
+var adjective = "1. nice милый,приятный 2. beautiful красивый 3. young молодой 4. old старый 5. big большой 6. different разный,непохожий,отличный(от_чего-то) 7. useful полезный 8. large крупный,большой 9. little маленький 10. every каждый,любой 11. important важный 12. small небольшой,маленький 13. basic основной,начальный,базовый 14. available доступный 15. popular популярный 16. cold холодный 17. hot жаркий,горячий 18. additional дополнительный 19. interesting интересный 20. funny смешной 21. strange странный 22. usual обычный 23. similar похожий 24. happy счастливый 25. healthy здоровый 26. traditional традиционный 27. strong сильный 28. weak слабый 29. strict строгий 30. angry злой 31. kind добрый 32. gorgeous изумительный,роскошный 33. wonderful чудесный 34. fast быстрый 35. successful успешный 36. cheap дешевый 37. expensive дорогой 38. poor бедный 39. rich богатый 40. polite вежливый,воспитанный 41. rude грубый 42. responsible ответственный 43. slow медленный 44. helpful предупредительный,отзывчивый 45. recent недавний 46. possible возможный 47. impossible невозможный 48. typical типичный 49. huge огромный,грандиозный 50. rare редкий";
+
+var verbMultiplier = 5,
+    adjectiveMultiplier = 3;
 
 var easyWordsArr = easyWordsStr.split(' '),
     easyWordsArrEng = [],
     easyWordsArrRus = [],
-    easyWordsLength = easyWordsArr.length / 5;
+    easyWordsLength = easyWordsArr.length / verbMultiplier;
 var mediumWordsArr = mediumWordsStr.split(' '),
     mediumWordsArrEng = [],
     mediumWordsArrRus = [],
-    mediumWordsLength = mediumWordsArr.length / 5;
+    mediumWordsLength = mediumWordsArr.length / verbMultiplier;
 var hardWordsArr = hardWordsStr.split(' '),
     hardWordsArrEng = [],
     hardWordsArrRus = [],
-    hardWordsLength = hardWordsArr.length / 5;
+    hardWordsLength = hardWordsArr.length / verbMultiplier;
+var adjectiveArr = adjective.split(' '),
+    adjectiveArrEng = [],
+    adjectiveArrRus = [],
+    adjectiveLength = adjectiveArr.length / adjectiveMultiplier;
 
 
-pushArray(easyWordsArr, easyWordsArrEng, easyWordsArrRus);
-pushArray(mediumWordsArr, mediumWordsArrEng, mediumWordsArrRus);
-pushArray(hardWordsArr, hardWordsArrEng, hardWordsArrRus);
-function pushArray(currentArr, engArr, rusArr){
-  for (var i = 1; i < currentArr.length; i = i+5) {
+pushArray(easyWordsArr, easyWordsArrEng, easyWordsArrRus, verbMultiplier);
+pushArray(mediumWordsArr, mediumWordsArrEng, mediumWordsArrRus, verbMultiplier);
+pushArray(hardWordsArr, hardWordsArrEng, hardWordsArrRus, verbMultiplier);
+pushArray(adjectiveArr, adjectiveArrEng, adjectiveArrRus, adjectiveMultiplier);
+function pushArray(currentArr, engArr, rusArr, multiplier){
+  for (var i = 1; i < currentArr.length; i = i + multiplier) {
     engArr.push(currentArr[i]);
     rusArr.push(currentArr[i+1]);
   };
 }
 
-console.log(easyWordsLength, mediumWordsLength, hardWordsLength, easyWordsArrEng, mediumWordsArrRus, hardWordsArrRus);
+
+console.log(easyWordsArrEng, mediumWordsArrRus, hardWordsArrRus, adjectiveArrEng);
 
 var mode = "",
     currentLength = null,
     rng = null,
-    order = false,       // eng || rus || mix
+    order = "eng_rus",       // eng || rus || mix
     orderValue = null,
-    // currectAsk = "",
-    // currectAnswer = "",
+                  // currectAsk = "",
+                  // currectAnswer = "",
     engEasy = [],
     ruEasy = [];
 
@@ -58,18 +68,32 @@ backToMenu.onclick = function(){
   screenMainMenu.style.display = "block";
   screenProgram.style.display = "none";
 }
-/*revers*/
+
+/* Order ENG-RUS RUS-ENG Mix */
 revers.onclick = function(){
-  if(order){
-    order = false;
-    revers.style.backgroundColor = "buttonface";
+  if(order == "eng_rus"){
+    order = "rus_eng";
+    revers.innerHTML = "rus-eng"
   }
-  else {
-    order = true;
-    revers.style.backgroundColor = "lightseagreen";
+  else if(order == "rus_eng"){
+    order = "mix";
+    revers.innerHTML = "mix"
   }
-  console.log(order)
+  else if(order == "mix"){
+    order = "eng_rus";
+    revers.innerHTML = "eng-rus"
+  }
+
+              // if(order){
+              //   order = false;
+              //   revers.style.backgroundColor = "buttonface";
+              // }
+              // else {
+              //   order = true;
+              //   revers.style.backgroundColor = "lightseagreen";
+              // }
 }
+
 /*select mode*/
 btnEasy.onclick = function(){
   mode = "easy-mode";
@@ -92,30 +116,49 @@ function displayTransition(){
   go();
 }
 
+// включение и выключение транскрипции (необходимо перелопатить Index)
+/*
+        var posttrans = [];
+        var transcription = [];
+        easyWordsArrEng.forEach(cut);
+        var indx1 = "";
+        var indx2 = "";
+        var transRes = "";
+        function cut(x){
+            if(x.includes("[") == true){
+            indx1 = x.indexOf("[");
+            indx2 = x.indexOf("]");
+            transRes = x.substring(indx1, indx2+1);
+            posttrans.push(x.replace(transRes, ''));
+            transcription.push(transRes);
+          }
+        }
+        easyWordsArrEng = transcription;
+*/
 
 function go(){
   blesRng();
   if (mode == "easy-mode"){
-    if (order == false || orderValue == 0){
+    if (order == "eng_rus" || (order == "mix" && orderValue == 0)){
       askWord.innerHTML = easyWordsArrEng[rng];
       correctAnswer = easyWordsArrRus[rng];
-    } else if (orderValue == 1){
+    } else if (order == "rus_eng" || (order == "mix" && orderValue == 1)){
       askWord.innerHTML = easyWordsArrRus[rng];
       correctAnswer = easyWordsArrEng[rng];
     }
   } else if (mode == "medium-mode"){
-    if (order == false || orderValue == 0){
+    if (order == "eng_rus" || (order == "mix" && orderValue == 0)){
       askWord.innerHTML = mediumWordsArrEng[rng];
       correctAnswer = mediumWordsArrRus[rng];
-    } else if (orderValue == 1){
+    } else if (order == "rus_eng" || (order == "mix" && orderValue == 1)){
       askWord.innerHTML = mediumWordsArrRus[rng];
       correctAnswer = mediumWordsArrEng[rng];
     }
   } else if (mode == "hard-mode"){
-    if (order == false || orderValue == 0){
+    if (order == "eng_rus" || (order == "mix" && orderValue == 0)){
       askWord.innerHTML = hardWordsArrEng[rng];
       correctAnswer = hardWordsArrRus[rng];
-    } else if (orderValue == 1){
+    } else if (order == "rus_eng" || (order == "mix" && orderValue == 1)){
       askWord.innerHTML = hardWordsArrRus[rng];
       correctAnswer = hardWordsArrEng[rng];
     }
