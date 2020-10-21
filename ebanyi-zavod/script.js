0e8496dd6c950c39c3fef56e6ff0c125259d77d1
grimpatron <43726927+grimpatron@users.noreply.github.com>
2020-10-21 22:30:06 +0300

    Add files via upload

---
+++ ebanyi-zavod/script.js
@@ -0,0 +1,15 @@
+/* Пароль на страницы */
+
+// function Accept() {
+//     var pass = document.getElementById('pass').value;
+//             win  = document.getElementById('pass-window');
+//             mask = document.getElementById('mask');
+//     if (pass == '228') {
+//             win.style.display='none';
+//             mask.style.display='none';
+//             wrap.style.filter="blur(0px)";
+//     } else { alert('Введенный Вами пароль неверен!'); }
+// }
+
+// https://javascriptobfuscator.com/Javascript-Obfuscator.aspx
+var _0x2167=["\x76\x61\x6C\x75\x65","\x70\x61\x73\x73","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x42\x79\x49\x64","\x70\x61\x73\x73\x2D\x77\x69\x6E\x64\x6F\x77","\x6D\x61\x73\x6B","\x32\x32\x38","\x64\x69\x73\x70\x6C\x61\x79","\x73\x74\x79\x6C\x65","\x6E\x6F\x6E\x65","\x66\x69\x6C\x74\x65\x72","\x62\x6C\x75\x72\x28\x30\x70\x78\x29","\u0412\u0432\u0435\u0434\u0435\u043D\u043D\u044B\u0439\x20\u0412\u0430\u043C\u0438\x20\u043F\u0430\u0440\u043E\u043B\u044C\x20\u043D\u0435\u0432\u0435\u0440\u0435\u043D\x21"];function Accept(){var _0x58d6x2=document[_0x2167[2]](_0x2167[1])[_0x2167[0]];win= document[_0x2167[2]](_0x2167[3]);mask= document[_0x2167[2]](_0x2167[4]);if(_0x58d6x2== _0x2167[5]){win[_0x2167[7]][_0x2167[6]]= _0x2167[8];mask[_0x2167[7]][_0x2167[6]]= _0x2167[8];wrap[_0x2167[7]][_0x2167[9]]= _0x2167[10]}else {alert(_0x2167[11])}}
