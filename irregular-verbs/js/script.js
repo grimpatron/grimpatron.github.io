1ef1577484d1709cd72396e0d46df80a79c06935
grimpatron <43726927+grimpatron@users.noreply.github.com>
2022-02-13 21:57:30 +0300

    Add files via upload

--- irregular-verbs/js/script.js
+++ irregular-verbs/js/script.js
@@ -1,113 +1,11 @@
-let list = [
-['be', 'was, were', 'been', 'быть, являться'], 
-['beat', 'beat', 'beaten', 'бить, колотить'], 
-['become', 'became', 'become', 'становиться'], 
-['begin', 'began', 'begun', 'начинать'],
-['bend', 'bent', 'bent', 'гнуть'],
-['bind', 'bound', 'bound', 'связывать'],
-  // ['bet', 'bet', 'bet', 'держать пари'], 
-['bite', 'bit', 'bitten', 'кусать'], 
-['blow', 'blew', 'blown', 'дуть, выдыхать'], 
-['break', 'broke', 'broken', 'ломать, разбивать, разрушать'], 
-['bring', 'brought', 'brought', 'приносить, привозить, доставлять'], 
-['build', 'built', 'built', 'строить, сооружать'], 
-['burn', 'burnt', 'burnt', 'жечь, гореть'],
-['buy', 'bought', 'bought', 'покупать, приобретать'], 
-['catch', 'caught', 'caught', 'ловить, поймать, схватить'], 
-['choose', 'chose', 'chosen', 'выбирать, избирать'], 
-['come', 'came', 'come', 'приходить, подходить'], 
-['cost', 'cost', 'cost', 'стоить, обходиться'], 
-['cut', 'cut', 'cut', 'резать, разрезать'], 
-  // ['deal', 'dealt', 'dealt', 'иметь дело, распределять'], 
-['dig', 'dug', 'dug', 'копать, рыть'], 
-['do', 'did', 'done', 'делать, выполнять'], 
-['draw', 'drew', 'drawn', 'рисовать, чертить'], 
-['dream', 'dreamt', 'dreamt', 'мечтать'], 
-['drink', 'drank', 'drunk', 'пить'], 
-['drive', 'drove', 'driven', 'ездить, подвозить'],
-['eat', 'ate', 'eaten', 'есть, поглощать, поедать'],
-['fall', 'fell', 'fallen', 'падать'],
-['feed', 'fed', 'fed', 'кормить'],
-['feel', 'felt', 'felt', 'чувствовать, ощущать'],
-['fight', 'fought', 'fought', 'драться, сражаться, воевать'],
-['find', 'found', 'found', 'находить, обнаруживать'],
-['fly', 'flew', 'flown', 'летать'],
-['forget', 'forgot', 'forgotten', 'забывать о (чём-либо)'],
-['forgive', 'forgave', 'forgiven', 'прощать'],
-['freeze', 'froze', 'frozen', 'замерзать, замирать'],
-['get', 'got', 'got', 'получать, добираться'],
-['give', 'gave', 'given', 'дать, подать, дарить'],
-['go', 'went', 'gone', 'идти, двигаться'],
-['grow', 'grew', 'grown', 'расти, вырастать'],
-['hang', 'hung', 'hung', 'вешать, развешивать, висеть'],
-['have', 'had', 'had', 'иметь, обладать'],
-['hear', 'heard', 'heard', 'слышать, услышать'],
-['hide', 'hid', 'hidden', 'прятать, скрывать'],
-['hit', 'hit', 'hit', 'ударять, поражать'],
-['hold', 'held', 'held', 'держать, удерживать, задерживать'],
-['hurt', 'hurt', 'hurt', 'ранить, причинять боль, ушибить'],
-['keep', 'kept', 'kept', 'хранить, сохранять, поддерживать'],
-['know', 'knew', 'known', 'знать, иметь представление'],
-['lay', 'laid', 'laid', 'класть, положить, покрывать'],
-['lead', 'led', 'led', 'вести за собой, сопровождать, руководить'],
-['leave', 'left', 'left', 'покидать, уходить, уезжать, оставлять'],
-['lend', 'lent', 'lent', 'одалживать, давать взаймы (в долг)'],
-['let', 'let', 'let', 'позволять, разрешать'],
-['lie', 'lay', 'lain', 'лежать'],
-['light', 'lit', 'lit', 'зажигать, светиться, освещать'],
-['lose', 'lost', 'lost', 'терять, лишаться, утрачивать'],
-['make', 'made', 'made', 'делать, создавать, изготавливать'],
-['mean', 'meant', 'meant', 'значить, иметь в виду, подразумевать'],
-['meet', 'met', 'met', 'встречать, знакомиться'],
-['pay', 'paid', 'paid', 'платить, оплачивать, рассчитываться'],
-['put', 'put', 'put', 'ставить, помещать, класть'],
-['read', 'read', 'read', 'читать, прочитать'],
-['ride', 'rode', 'ridden', 'ехать верхом, кататься'],
-['ring', 'rang', 'rung', 'звенеть, звонить'],
-['rise', 'rose', 'risen', 'восходить, вставать, подниматься'],
-['run', 'ran', 'run', 'бежать, бегать'],
-['say', 'said', 'said', 'говорить, сказать, произносить'],
-['see', 'saw', 'seen', 'видеть'],
-['seek', 'sought', 'sought', 'искать, разыскивать'],
-['sell', 'sold', 'sold', 'продавать, торговать'],
-['send', 'sent', 'sent', 'посылать, отправлять, отсылать'],
-['set', 'set', 'set', 'устанавливать, задавать, назначать'],
-['shake', 'shook', 'shaken', 'трясти, встряхивать'],
-['shine', 'shone', 'shone', 'светить, сиять, озарять'],
-['shoot', 'shot', 'shot', 'стрелять'],
-['show', 'showed', 'shown, showed', 'показывать'],
-['shut', 'shut', 'shut', 'закрывать, запирать, затворять'],
-['sing', 'sang', 'sung', 'петь, напевать'],
-['sink', 'sank', 'sunk', 'тонуть, погружаться'],
-['sit', 'sat', 'sat', 'сидеть, садиться'],
-['sleep', 'slept', 'slept', 'спать'],
-['speak', 'spoke', 'spoken', 'говорить, разговаривать, высказываться'],
-['spend', 'spent', 'spent', 'тратить, расходовать, проводить (время)'],
-['stand', 'stood', 'stood', 'стоять'],
-['steal', 'stole', 'stolen', 'воровать, красть'],
-['stick', 'stuck', 'stuck', 'втыкать, приклеивать'],
-['strike', 'struck', 'struck, stricken', 'ударять, бить, поражать'],
-['swear', 'swore', 'sworn', 'клясться, присягать'],
-['sweep', 'swept', 'swept', 'мести, подметать, смахивать'],
-['swim', 'swam', 'swum', 'плавать, плыть'],
-['swing', 'swung', 'swung', 'качаться, вертеться'],
-['take', 'took', 'taken', 'брать, хватать, взять'],
-['teach', 'taught', 'taught', 'учить, обучать'],
-['tear', 'tore', 'torn', 'рвать, отрывать'],
-['tell', 'told', 'told', 'рассказывать'],
-['think', 'thought', 'thought', 'думать, мыслить, размышлять'],
-['throw', 'threw', 'thrown', 'бросать, кидать, метать'],
-['understand', 'understood', 'understood', 'понимать, постигать'],
-['wake', 'woke', 'woken', 'просыпаться, будить'],
-['wear', 'wore', 'worn', 'носить (одежду)'],
-['win', 'won', 'won', 'победить, выиграть'],
-['write', 'wrote', 'written', 'писать, записывать']];
+let list = [['be', 'was, were', 'been', 'быть, являться', '3'], ['beat', 'beat', 'beaten', 'бить, колотить', '2233'], ['become', 'became', 'become', 'становиться', '261'], ['begin', 'began', 'begun', 'начинать', '364'], ['bend', 'bent', 'bent', 'гнуть', '4708'], ['bet', 'bet', 'bet', 'держать пари, делать ставку', '5979'], ['bind', 'bound', 'bound', 'связывать', '3972'], ['bite', 'bit', 'bitten', 'кусать', '5998'], ['blow', 'blew', 'blown', 'дуть, выдыхать', '3288'], ['break', 'broke', 'broken', 'ломать, разбивать, разрушать', '1003'], ['bring', 'brought', 'brought', 'приносить, привозить, доставлять', '413'], ['build', 'built', 'built', 'строить, сооружать', '805'], ['burn', 'burnt', 'burnt', 'жечь, гореть', '3266'], ['burst', 'burst', 'burst', 'разрываться,лопаться', '6675'], ['buy', 'bought', 'bought', 'покупать, приобретать', '770'], ['catch', 'caught', 'caught', 'ловить, поймать, схватить', '1257'], ['choose', 'chose', 'chosen', 'выбирать, избирать', '1180'], ['come', 'came', 'come', 'приходить, подходить', '133'], ['cost', 'cost', 'cost', 'стоить, обходиться', '907'], ['cut', 'cut', 'cut', 'резать, разрезать', '950'], ['deal', 'dealt', 'dealt', 'иметь дело, распределять', '1265'], ['dig', 'dug', 'dug', 'копать, рыть', '5382'], ['do', 'did', 'done', 'делать, выполнять', '34'], ['draw', 'drew', 'drawn', 'рисовать, чертить', '1043'], ['dream', 'dreamt', 'dreamt', 'мечтать', '2553'], ['drink', 'drank', 'drunk', 'пить', '2318'], ['drive', 'drove', 'driven', 'ездить, подвозить', '1080'], ['eat', 'ate', 'eaten', 'есть, поглощать, поедать', '1174'], ['fall', 'fell', 'fallen', 'падать', '776'], ['feed', 'fed', 'fed', 'кормить', '3651'], ['feel', 'felt', 'felt', 'чувствовать, ощущать', '272'], ['fight', 'fought', 'fought', 'драться, сражаться, воевать', '1500'], ['find', 'found', 'found', 'находить, обнаруживать', '191'], ['flee', 'fled', 'fled', 'убегать, спасаться бегством', '6353'], ['fly', 'flew', 'flown', 'летать', '1868'], ['forbid', 'forbade', 'forbidden', 'запрещать', '9647'], ['forget', 'forgot', 'forgotten', 'забывать о (чём-либо)', '1671'], ['forgive', 'forgave', 'forgiven', 'прощать', '7199'], ['freeze', 'froze', 'frozen', 'замерзать, замирать', '6145'], ['get', 'got', 'got', 'получать, добираться', '81'], ['give', 'gave', 'given', 'дать, подать, дарить', '171'], ['go', 'went', 'gone', 'идти, двигаться', '73'], ['grow', 'grew', 'grown', 'расти, вырастать', '890'], ['hang', 'hung', 'hung', 'вешать, развешивать, висеть', '1992'], ['have', 'had', 'had', 'иметь, обладать', '15'], ['hear', 'heard', 'heard', 'слышать, услышать', '440'], ['hide', 'hid', 'hidden', 'прятать, скрывать', '2843'], ['hit', 'hit', 'hit', 'ударять, поражать', '1489'], ['hold', 'held', 'held', 'держать, удерживать, задерживать', '398'], ['hurt', 'hurt', 'hurt', 'ранить, причинять боль, ушибить', '3042'], ['keep', 'kept', 'kept', 'хранить, сохранять, поддерживать', '332'], ['know', 'knew', 'known', 'знать, иметь представление', '96'], ['lay', 'laid', 'laid', 'класть, положить, покрывать', '2078'], ['lead', 'led', 'led', 'вести за собой, сопровождать, руководить', '588'], ['learn', 'learnt', 'learnt', 'учить(ся)', '715'], ['leave', 'left', 'left', 'покидать, уходить, уезжать, оставлять', '278'], ['lend', 'lent', 'lent', 'одалживать, давать взаймы (в долг)', '6357'], ['let', 'let', 'let', 'позволять, разрешать', '468'], ['lie', 'lay', 'lain', 'лежать', '1178'], ['light', 'lit', 'lit', 'зажигать, светиться, освещать', '2944'], ['lose', 'lost', 'lost', 'терять, лишаться, утрачивать', '627'], ['make', 'made', 'made', 'делать, создавать, изготавливать', '89'], ['mean', 'meant', 'meant', 'значить, иметь в виду, подразумевать', '279'], ['meet', 'met', 'met', 'встречать, знакомиться', '542'], ['overcome', 'overcame', 'overcome', 'преодолеть,побороть', '5280'], ['pay', 'paid', 'paid', 'платить, оплачивать, рассчитываться', '531'], ['put', 'put', 'put', 'ставить, помещать, класть', '270'], ['read', 'read', 'read', 'читать, прочитать', '668'], ['ride', 'rode', 'ridden', 'ехать верхом, кататься', '3121'], ['ring', 'rang', 'rung', 'звенеть, звонить', '3636'], ['rise', 'rose', 'risen', 'восходить, вставать, подниматься', '1374'], ['run', 'ran', 'run', 'бежать, бегать', '417'], ['say', 'said', 'said', 'говорить, сказать, произносить', '51'], ['see', 'saw', 'seen', 'видеть', '115'], ['seek', 'sought', 'sought', 'искать, разыскивать', '1263'], ['sell', 'sold', 'sold', 'продавать, торговать', '915'], ['send', 'sent', 'sent', 'посылать, отправлять, отсылать', '787'], ['set', 'set', 'set', 'устанавливать, задавать, назначать', '513'], ['shake', 'shook', 'shaken', 'трясти, встряхивать', '2000'], ['shine', 'shone', 'shone', 'светить, сиять, озарять', '6723'], ['shoot', 'shot', 'shot', 'стрелять', '1940'], ['show', 'showed', 'shown, showed', 'показывать', '329'], ['shut', 'shut', 'shut', 'закрывать, запирать, затворять', '3277'], ['sing', 'sang', 'sung', 'петь, напевать', '2529'], ['sink', 'sank', 'sunk', 'тонуть, погружаться', '5442'], ['sit', 'sat', 'sat', 'сидеть, садиться', '565'], ['sleep', 'slept', 'slept', 'спать', '2534'], ['slide', 'slid', 'slid', 'скользить  ', '4887'], ['smell', 'smelt', 'smelt', 'пахнуть', '5147'], ['speak', 'spoke', 'spoken', 'говорить, разговаривать, высказываться', '702'], ['spend', 'spent', 'spent', 'тратить, расходовать, проводить (время)', '740'], ['split', 'split', 'split', 'раскалывать, расщеплять', '5617'], ['spread', 'spread', 'spread', 'распространять', '2917'], ['spring', 'sprang', 'sprung', 'прыгать', '5688'], ['stand', 'stood', 'stood', 'стоять', '552'], ['steal', 'stole', 'stolen', 'воровать, красть', '3786'], ['stick', 'stuck', 'stuck', 'втыкать, приклеивать', '2557'], ['strike', 'struck', 'struck, stricken', 'ударять, бить, поражать', '2346'], ['swear', 'swore', 'sworn', 'клясться, присягать', '6618'], ['sweep', 'swept', 'swept', 'мести, подметать, смахивать', '5314'], ['swim', 'swam', 'swum', 'плавать, плыть', '5872'], ['swing', 'swung', 'swung', 'качаться, вертеться', '4962'], ['take', 'took', 'taken', 'брать, хватать, взять', '114'], ['teach', 'taught', 'taught', 'учить, обучать', '1487'], ['tear', 'tore', 'torn', 'рвать, отрывать', '4174'], ['tell', 'told', 'told', 'рассказывать', '213'], ['think', 'thought', 'thought', 'думать, мыслить, размышлять', '117'], ['throw', 'threw', 'thrown', 'бросать, кидать, метать', '1549'], ['understand', 'understood', 'understood', 'понимать, постигать', '708'], ['wake', 'woke', 'woken', 'просыпаться, будить', '4044'], ['wear', 'wore', 'worn', 'носить (одежду)', '1202'], ['win', 'won', 'won', 'победить, выиграть', '748'], ['write', 'wrote', 'written', 'писать, записывать', '436']]
 
 
 let rus = document.querySelector("#rus");
 let word = document.querySelector("#word");
 let check = document.querySelector("#check");
 let input = document.querySelector("#answer_input");
+let frequency = document.querySelector("#frequency");
 
 
 let cnt = -1;
@@ -119,32 +17,38 @@ check.addEventListener( 'click', () => {
     v1.innerHTML = list[cnt][0];
     v2.innerHTML = list[cnt][1];
     v3.innerHTML = list[cnt][2];
-    // v2.classList.add('animate__zoomInRight');
-    // v1.classList.add('animate__zoomInRight');
-    // v3.classList.add('animate__zoomInRight');
     flag = 1;
   } else {
     step();
-    // input.value = '';
     v1.innerHTML = '';
     v2.innerHTML = '';
     v3.innerHTML = '';
-    // v1.classList.remove('animate__zoomInRight');
-    // v2.classList.remove('animate__zoomInRight');
-    // v3.classList.remove('animate__zoomInRight');
     flag = 0;
   }
 });
 
 step();
 function step() {
-  // input.focus();
   if (cnt <= list.length - 2) {
     cnt++;
-    // word.innerHTML = list[cnt][0];
     rus.innerHTML = list[cnt][3];
   } else {
     word.innerHTML = "The end!";
     input.disabled = true;
   }
+}
+
+frequency.addEventListener("click", () => {
+  list.sort(
+    function (a, b) {
+    if (parseInt(a[4]) > parseInt(b[4])) { return 1; }
+    if (parseInt(a[4]) < parseInt(b[4])) { return -1; }
+    });
+    console.log(list);
+});
+
+function reset() {
+  cnt = -1;
+  flag = 0;
+  step();
 }
